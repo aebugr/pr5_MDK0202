4b4de77502da4e5f6bb4d75ccd875c9c5ef3abdf
HP <HP@DESKTOP-T6A34MC>
2024-12-17 00:40:34 +0500

    разработаны метод для подключения клиента к серверу + метод отключения + метод для обработки команд клиента. бугрина

--- Server/Program.cs
+++ Server/Program.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Server
 {
-    public class Program
+    internal class Program
     {
         static IPAddress Ip;
         static int Port;
         static int MaxClient;
         static int Duration;
-        // Список всех подключенных клиентов (с хранением информации о каждом клиенте)
+
         static List<string> ClientTokens = new List<string>();
         static List<DateTime> ClientConnections = new List<DateTime>();
         static void Main(string[] args)
@@ -67,7 +68,7 @@ namespace Server
                 Console.ForegroundColor = ConsoleColor.Green;
                 MaxClient = int.Parse(Console.ReadLine());
                 Console.ForegroundColor = ConsoleColor.White;
-                Console.Write("Укажите время жизни токена (в секундах): ");
+                Console.Write("Укажите время использования токена (в секундах): ");
                 Console.ForegroundColor = ConsoleColor.Green;
                 Duration = int.Parse(Console.ReadLine());
 
@@ -82,6 +83,74 @@ namespace Server
             Console.Write("Чтобы изменить настройки, используйте команду: ");
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("/config");
+        }
+        static void Connect()
+        {
+            IPEndPoint endPoint = new IPEndPoint(Ip, Port);
+            Socket SocketListener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            SocketListener.Bind(endPoint);
+            SocketListener.Listen(10); 
+            while (true)
+            {
+                Socket Handler = SocketListener.Accept();
+                byte[] Bytes = new byte[10485760];
+                int ByteRec = Handler.Receive(Bytes);
+                string Message = Encoding.UTF8.GetString(Bytes, 0, ByteRec);
+                string Response = SetCommandClient(Message);
+                Handler.Send(Encoding.UTF8.GetBytes(Response));
+            }
+        }
+        static void Disconnect(string command)
+        {
+            try
+            {
+                string Token = command.Replace("/disconnect ", "");
+                int index = ClientTokens.IndexOf(Token);
+                if (index != -1)
+                {
+                    ClientTokens.RemoveAt(index);
+                    ClientConnections.RemoveAt(index);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Клиент с токеном {Token} был отключен");
+                }
+            }
+            catch (Exception exp)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Ошибка: " + exp.Message);
+            }
+        }
+        static string SetCommandClient(string Command)
+        {
+            if (Command == "/token")
+            {
+                if (ClientTokens.Count < MaxClient)
+                {
+                    string newToken = GenerateToken();
+                    ClientTokens.Add(newToken);
+                    ClientConnections.Add(DateTime.Now);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Новый клиент подключен: {newToken}");
+                    return newToken; 
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Нет места для нового клиента на сервере");
+                    return "/limit";
+                }
+            }
+            else
+            {
+                int index = ClientTokens.IndexOf(Command);
+                return index != -1 ? "/connect" : "/disconnect";
+            }
+        }
+        static string GenerateToken()
+        {
+            Random random = new Random();
+            string Chars = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm123456789";
+            return new string(Enumerable.Repeat(Chars, 15).Select(x => x[random.Next(Chars.Length)]).ToArray());
         }
     }
 }
